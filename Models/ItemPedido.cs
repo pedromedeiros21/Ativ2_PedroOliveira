d6d46871ded5412ed2ac7ee69a7ad1990a9b7527
Pedro Henrique Medeiros <pedro.tramandai@gmail.com>
2021-09-07 16:07:01 -0300

    Arrumando identação

--- Models/ItemPedido.cs
+++ Models/ItemPedido.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
+public class ItemPedido
+{
+    public string Descricao { get; set; }
+    public double ValorUnitario { get; set; }
+    public double Quantidade { get; set; }
 
-public class ItemPedido{
-
-public string Descricao { get; set; }
-public double ValorUnitario { get; set; }
-public double Quantidade { get; set; }
-
-public double Total { get { return ValorUnitario * Quantidade; } }
-
+    public double Total { get { return ValorUnitario * Quantidade; } }
 }
